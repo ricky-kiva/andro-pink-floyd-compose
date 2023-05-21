f8c6e362849717439fc3930802fc52f52ea722e3
ricky-kiva <ricky.kiva@gmail.com>
2023-05-21 22:17:44 +0700

    add AlbumListItem Component

--- app/src/main/java/com/rickyslash/pinkfloydcompose/ui/component/AlbumListItem.kt
+++ app/src/main/java/com/rickyslash/pinkfloydcompose/ui/component/AlbumListItem.kt
@@ -1,18 +1,18 @@
 package com.rickyslash.pinkfloydcompose.ui.component
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,45 +22,73 @@ import com.rickyslash.pinkfloydcompose.R
 
 @Composable
 fun AlbumListItem(imageUrl: String, title: String, release: String, onItemClick: () -> Unit) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Column(
+    Column{
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
-                .padding(end = 16.dp)
+                .padding(vertical = 24.dp)
+                .fillMaxWidth()
         ) {
-            Text(
-                text = title,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Text(
-                text = release
-            )
+            Column {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(bottom = 4.dp)
+                ) {
+                    Text(
+                        text = title,
+                        fontSize = 16.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowRight,
+                        contentDescription = stringResource(R.string.desc_arrow),
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .size(11.dp)
+                    )
+                }
+                Text(
+                    text = release,
+                    fontSize = 11.sp,
+                    color = (MaterialTheme.colors.onSecondary).copy(alpha = 0.5f)
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        shape = CircleShape,
+                        color = (MaterialTheme.colors.onSecondary).copy(alpha = 0.5f)
+                    )
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = stringResource(R.string.desc_album_name, title),
+                    placeholder = painterResource(R.drawable.img_placeholder_dsod),
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(12.dp)
+                        .clip(CircleShape)
+                )
+            }
         }
-        AsyncImage(
-            model = imageUrl,
-            contentDescription = stringResource(R.string.desc_album_name, title),
-            placeholder = painterResource(R.drawable.img_placeholder_dsod),
-            modifier = Modifier
-                .size(64.dp)
-                .clip(CircleShape)
-        )
-        Divider()
+        Divider(color = MaterialTheme.colors.onSecondary.copy(alpha = 0.5f))
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun AlbumListItemPreview() {
-    PinkFloydComposeTheme {
-        AlbumListItem(
-            "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
-            "Dark Side of the Moon",
-            "March 1, 1973"
-        ) {}
+    PinkFloydComposeTheme(darkTheme = true) {
+        Surface {
+            AlbumListItem(
+                "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+                "Dark Side of the Moon",
+                "March 1, 1973"
+            ) {}
+        }
     }
 }
